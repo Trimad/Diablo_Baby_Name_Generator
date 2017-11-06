c3f851a0ed9956aebc49e5c3d3f03d7e49703f7c
Trimad <mattedinnards@gmail.com>
2017-11-06 05:48:11 -1000

    yes

--- sketch.js
+++ sketch.js
@@ -1,48 +1,46 @@
-let babynames;
-let buildIt;
-let bg;
-let myfont;
-let button;
-let song;
+var eatSound;
+var startOverSound;
+var helloSound;
+var song;
+var shrine;
+var buildIt = "";
+var bg;
 
 function preload() {
-
   babynames = loadJSON("babynames.json");
-  bg = loadImage("bg.jpg");
+  bg = loadImage("diablo_menu.jpg");
+  shrine = loadImage("shrine.png");
+
   myFont = loadFont('diablo.ttf');
-  //song = loadSound('assets/intro.mp3');
+  song = loadSound("intro.mp3");
 }
 
 function setup() {
+  song.playMode('restart');
+  song.play();
   textFont(myFont);
   createCanvas(windowWidth, windowHeight);
-  //song.play();
+
   stroke(255);
   fill(255);
   background(bg);
-  button = createButton('NEW BABY NAME');
-  button.position(width-500, 100);
-  button.style("font-size", "32px");
-  let col = color(100,0,00);
-  button.style("background-color", col);
+
+  textAlign(CENTER, CENTER);
+
+
+  button = createButton('GENERATE');
+  button.position(width / 2 - 60, height * .4);
+  button.style("font-size", "16px");
+  button.style("font-family", 'Orator Std');
+  button.style("background-color", red);
   button.mousePressed(newName);
-
-
-}
-
-function windowResized() {
-
-  resizeCanvas(windowWidth, windowHeight);
-  background(bg);
-  text(buildIt, width / 2, height / 2 - 24);
-
 }
 
 function newName() {
 
-  let randomPrefix = floor(random(0, babynames.prefix.length));
-  let randomSuffix = floor(random(0, babynames.suffix.length));
-  let randomAppelation = floor(random(0, babynames.appelation.length));
+  var randomPrefix = floor(random(0, babynames.prefix.length));
+  var randomSuffix = floor(random(0, babynames.suffix.length));
+  var randomAppelation = floor(random(0, babynames.appelation.length));
 
   background(bg);
   buildIt = "";
@@ -50,7 +48,45 @@ function newName() {
   buildIt += babynames.suffix[randomSuffix] + " ";
   buildIt += babynames.appelation[randomAppelation];
 
-  textSize(48);
-  text(buildIt, width / 2, height / 2 - 24);
+
+
 
 }
+
+function windowResized() {
+
+  resizeCanvas(windowWidth, windowHeight);
+  //background(bg);
+  button.position(width / 2 - 70, height * .4);
+}
+
+function draw() {
+
+  var shrineWidth = 71;
+  var shrineHeight = 121;
+  background(bg);
+
+  textSize(32);
+  text("NAME YOUR LEGACY", width / 2, height * 0.33);
+
+  textSize(width / buildIt.length);
+  text(buildIt, width / 2, height * .5);
+
+  //image(shrine, width / 2, height / 2);
+  //image(img,dx,dy,dWidth,dHeight,sx,sy,[sWidth],[sHeight])
+  //image(shrine, width / 2 - (shrineWidth / 2), height / 2, shrineWidth, shrineHeight);
+  image(shrine, width / 2 - (shrineWidth / 2), height / 2, shrineHeight, shrineWidth, width/2, height/2, shrineWidth, shrineHeight);
+
+}
+/*
+  
+    if (frameCount % 60 >= 20 && frameCount % 60 < 40) {
+
+      image(playerSpriteSheet, this.position.x, this.position.y, playerSize, playerSize, sx + sw, sy + sh * 2, sw, sh);
+    }
+    if (frameCount % 60 >= 40 && frameCount % 60 <= 60) {
+
+      image(playerSpriteSheet, this.position.x, this.position.y, playerSize, playerSize, sx + sw * 2, sy + sh * 2, sw, sh);
+    }
+
+}*/
